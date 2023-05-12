0898b153729d725313f5e77fd37cf5f6770faec8
ofafax <philipofafa@gmail.com>
2023-05-12 06:54:19 -0700

    task 2 corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,16 @@
 
 if __name__ == "__main__":
 
-	import sys
-	q = len(sys.argv) - 1
+    import sys
+    q = len(sys.argv) - 1
 
-	if q == 0:
-		print("{} arguments.".format(q))
-	elif q == 1:
-		print("{} argument:".format(q))
-	else:
-		print("{} arguments:".format(q))
-		q = 1
-		for arg in argv[1:]:
-			print("{}: {}".format(q, arg))
-			q += 1
-
+    if q == 0:
+        print("{} arguments.".format(q))
+    elif q == 1:
+        print("{} argument:".format(q))
+    else:
+        print("{} arguments:".format(q))
+        q = 1
+        for arg in sys.argv[1:]:
+            print("{}: {}".format(q, arg))
+            q += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     import sys
 
     answer = 0
-    for arg in sys.argv[1:]:
-        answer += int(arg)
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            answer += int(arg)
 
         print(answer)
