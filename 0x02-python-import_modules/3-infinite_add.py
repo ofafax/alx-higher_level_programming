64bd4d1913cbad2c189dad96f91ecbdb2f42ed8e
ofafax <philipofafa@gmail.com>
2023-05-12 06:15:25 -0700

    infinite correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     import sys
 
     answer = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            answer += int(arg)
+    for arg in sys.argv[1:]:
+        answer += int(arg)
 
         print(answer)
