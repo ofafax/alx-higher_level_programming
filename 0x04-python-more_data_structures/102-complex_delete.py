e0e1d954458e130cbbc6ac80ffad457e91f36636
ofafax <philipofafa@gmail.com>
2023-05-17 19:37:46 -0700

    correction

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    for x in list(a_dictionary.keys()):
+        if a_dictionary[x] == value:
+            del a_dictionary[x]
+    return a_dictionary
