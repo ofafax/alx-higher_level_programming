4486fa656347d057f881fc28e862abd55b0f1c7c
ofafax <philipofafa@gmail.com>
2023-05-17 19:34:08 -0700

    correct

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    biggest = max(a_dictionary, key=a_dictionary.get)
+    biggest = max(a_dictionary, key=lambda x: a_dictionary[x])
     return = biggest
