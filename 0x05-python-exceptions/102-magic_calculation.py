540e05be6e39e8584009dbe4b52efeec3d254e06
ofafax <philipofafa@gmail.com>
2023-05-29 19:29:13 -0700

    corrections

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    ans = 0
+    result = 0
     for x in range(1, 3):
         try:
             if (x > a):
                 raise Exception("Too far")
             else:
-                ans += (a ** b) / x
+                result += (a ** b) / x
         except Exception:
-            ans = a + b
+            result = a + b
         break
-    return ans
+    return result
