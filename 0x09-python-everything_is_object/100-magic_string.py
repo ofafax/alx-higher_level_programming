44b3d06de63bec39e838235b3be8862f507c8166
ofafax <philipofafa@gmail.com>
2023-06-06 19:45:33 -0700

    extras

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(jina=[]):
+    jina.append("BestSchool")
+    return ", ".join(jina)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines locked class"""
+
+
+class LockedClass:
+    """locked class"""
+
+    __slots__ = ['first_name']
